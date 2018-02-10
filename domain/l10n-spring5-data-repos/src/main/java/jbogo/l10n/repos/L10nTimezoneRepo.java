a471b13278d5794cc2b4f4ba4b310f91326d81d0
Konstantinos Filios <konfilios@gmail.com>
2018-02-10 17:24:05 +0200

    Created spring5 data repos

---
+++ domain/l10n-spring5-data-repos/src/main/java/jbogo/l10n/repos/L10nTimezoneRepo.java
@@ -0,0 +1,11 @@
+package jbogo.l10n.repos;
+
+import jbogo.l10n.entities.L10nTimezone;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ *
+ */
+public interface L10nTimezoneRepo extends PagingAndSortingRepository<L10nTimezone, Short>
+{
+}
