24831af068cdd5928c5555167fa626ccb59db7d8
Konstantinos Filios <konfilios@gmail.com>
2018-05-31 09:48:00 +0300

    Added temporal progression

---
+++ java8-ext/src/test/java/org/j4work/java8/time/TemporalProgressionTest.java
@@ -0,0 +1,76 @@
+package org.j4work.java8.time;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.YearMonth;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TemporalProgressionTest {
+    @Test
+    public void testUpToLocalDate() {
+        final TemporalProgression<LocalDate> progression = TemporalProgression.upTo(
+            LocalDate.of(2015, 2, 27),
+            LocalDate.of(2015, 3, 1),
+            Period.ofDays(1)
+        );
+
+        assertThat(progression)
+            .containsExactlyInAnyOrder(
+                LocalDate.of(2015, 2, 27),
+                LocalDate.of(2015, 2, 28),
+                LocalDate.of(2015, 3, 1)
+            );
+    }
+
+    @Test
+    public void testDownToLocalDate() {
+        final TemporalProgression<LocalDate> progression = TemporalProgression.downTo(
+            LocalDate.of(2015, 3, 1),
+            LocalDate.of(2015, 2, 27),
+            Period.ofDays(1)
+        );
+
+        assertThat(progression)
+            .containsExactlyInAnyOrder(
+                LocalDate.of(2015, 3, 1),
+                LocalDate.of(2015, 2, 28),
+                LocalDate.of(2015, 2, 27)
+            );
+    }
+
+    @Test
+    public void testUpToYearMonth() {
+        final TemporalProgression<YearMonth> progression = TemporalProgression.upTo(
+            YearMonth.of(2015, 12),
+            YearMonth.of(2016, 1),
+            Period.ofMonths(1)
+        );
+
+        assertThat(progression)
+            .containsExactlyInAnyOrder(
+                YearMonth.of(2015, 12),
+                YearMonth.of(2016, 1)
+            );
+
+    }
+
+    @Test
+    public void testDownToYearMonth() {
+        final TemporalProgression<YearMonth> progression = TemporalProgression.downTo(
+            YearMonth.of(2016, 1),
+            YearMonth.of(2015, 12),
+            Period.ofMonths(1)
+        );
+
+        assertThat(progression)
+            .containsExactlyInAnyOrder(
+                YearMonth.of(2016, 1),
+                YearMonth.of(2015, 12)
+            );
+
+    }
+
+}
