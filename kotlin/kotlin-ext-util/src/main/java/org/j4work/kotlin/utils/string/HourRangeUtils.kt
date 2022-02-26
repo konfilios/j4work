697af1799c787a37d11942e4fecd338b5b7fada1
Konstantinos Filios <konfilios@gmail.com>
2022-02-26 15:24:55 +0200

    Added time ranges

--- kotlin/kotlin-ext-util/src/main/java/org/j4work/kotlin/utils/string/HourRangeUtils.kt
+++ kotlin/kotlin-ext-util/src/main/java/org/j4work/kotlin/utils/string/HourRangeUtils.kt
@@ -4,17 +4,20 @@ import com.google.common.collect.BoundType
 import com.google.common.collect.Range
 import com.google.common.collect.RangeSet
 import com.google.common.collect.TreeRangeSet
+import java.time.LocalTime
 
 /**
  * Convert hour range strings to [RangeSet] objects.
  */
 object HourRangeUtils {
-    private val hourRangeSymbols = Regex("\\d[-/\\d\\s]+")
+    private val timeRegex = "\\d+(\\.\\d+)?"
+    private val timeWithSpacesRegex = "\\s*${timeRegex}\\s*"
+    private val hourRangeSymbols = Regex("(/?${timeWithSpacesRegex}-${timeWithSpacesRegex})+")
 
     /**
      * Is given string a parseable hour range?
      */
-    fun isHourRange(s: String) =
+    fun isTimeRange(s: String) =
         hourRangeSymbols.matches(s)
 
     /**
@@ -29,12 +32,12 @@ object HourRangeUtils {
      * @param rangeSetString The string to parse
      * @param infimumStartHour Infimum start hour for am/pm implicit conversion
      */
-    fun parseHourRangeSet(
+    fun parseTimeRangeSet(
         rangeSetString: String,
         infimumStartHour: Int = 0
-    ): RangeSet<Int> {
-        val rangeSet = TreeRangeSet.create<Int>()
+    ): TreeRangeSet<LocalTime> {
         var minimumStartHour = infimumStartHour
+        val timeRangeSet = TreeRangeSet.create<LocalTime>()
 
         for (rangeString in rangeSetString.split("/")) {
             val (startString, endString) =
@@ -42,47 +45,81 @@ object HourRangeUtils {
                     ?: throw Exception("Could not parse time range $rangeString")
 
             // Parse start hour and add 12 hours if it's below threshold
-            val start = parseStartHour(startString, minimumStartHour, rangeSetString)
+            val startTime = parseStartHour(startString, minimumStartHour, rangeSetString)
 
             // Parse end hour and 12 hours if it's below start hour
-            val end = parseEndHour(endString, start, rangeSetString)
+            val endTime = parseEndHour(endString, startTime.hour, rangeSetString)
 
-            // Create range and add it to the set
-            rangeSet.add(Range.closedOpen(start, end))
+            timeRangeSet.add(Range.closedOpen(startTime, endTime))
 
             // Update minimum start hour so next range is properly evaluated
-            minimumStartHour = end
+            minimumStartHour = endTime.hour
         }
 
-        return rangeSet
+        return timeRangeSet
+    }
+
+    fun timeRangeSetToHourRangeSet(timeRangeSet: RangeSet<LocalTime>): TreeRangeSet<Int> {
+        val hourRangeSet = TreeRangeSet.create<Int>()
+        for (timeRange in timeRangeSet.asRanges()) {
+            // Create range and add it to the set
+            hourRangeSet.add(Range.closedOpen(timeRange.lowerEndpoint().hour, timeRange.upperEndpoint().hour))
+        }
+        return hourRangeSet
+    }
+
+    fun hourRangeSetToTimeRangeSet(hourRangeSet: RangeSet<Int>): TreeRangeSet<LocalTime> {
+        val timeRangeSet = TreeRangeSet.create<LocalTime>()
+        for (hourRange in hourRangeSet.asRanges()) {
+            // Create range and add it to the set
+            timeRangeSet.add(Range.closedOpen(LocalTime.of(hourRange.lowerEndpoint(), 0),
+                LocalTime.of(hourRange.upperEndpoint(), 0)))
+        }
+        return timeRangeSet
     }
 
     private fun parseEndHour(
         endString: String,
         minValue: Int,
         rangeSetString: String
-    ): Int {
-        val end = endString.trim().toInt().let {
+    ): LocalTime {
+        val (endHourInt: Int, endMinuteInt: Int) = parseTime(endString)
+
+        val endHour = endHourInt.let {
             if (it < minValue) it + 12 else it
         }
-        require(end in 0..23, {
+        require(endHour in 0..23, {
             "Could not parse end hour '$endString' in '$rangeSetString'"
         })
-        return end
+        return LocalTime.of(endHour, endMinuteInt)
     }
 
     private fun parseStartHour(
         startString: String,
-        minValue: Int,
+        minHour: Int,
         rangeSetString: String
-    ): Int {
-        val start = startString.trim().toInt().let {
-            if (it < minValue) it + 12 else it
+    ): LocalTime {
+        val (startHourInt: Int, startMinuteInt: Int) = parseTime(startString)
+
+        val startHour = startHourInt.let {
+            if (it < minHour) it + 12 else it
         }
-        require(start in 0..23, {
+        require(startHour in 0..23, {
             "Could not parse start hour '$startString' in '$rangeSetString'"
         })
-        return start
+        return LocalTime.of(startHour, startMinuteInt)
+    }
+
+    private fun parseTime(timeString: String): Pair<Int, Int> {
+        val timeElements = timeString.trim().split(".")
+
+        return if (timeElements.size > 1) {
+            Pair(timeElements[0].toInt(), timeElements[1].toInt())
+        } else if (timeElements.isNotEmpty()) {
+            Pair(timeElements[0].toInt(), 0)
+        } else {
+            throw IllegalArgumentException("Could not parse '$timeString' into hours and minutes")
+        }
     }
 
     fun measure(rangeSet: RangeSet<Int>) =
--- kotlin/kotlin-ext-util/src/test/java/org/j4work/kotlin/utils/string/HourRangeUtilsTest.kt
+++ kotlin/kotlin-ext-util/src/test/java/org/j4work/kotlin/utils/string/HourRangeUtilsTest.kt
@@ -2,50 +2,125 @@ package org.j4work.kotlin.utils.string
 
 import org.assertj.core.api.Assertions.assertThat
 import org.j4work.kotlin.extguava.com.google.common.collect.TreeRangeSets
-import org.j4work.kotlin.utils.string.HourRangeUtils.parseHourRangeSet
+import org.j4work.kotlin.utils.string.HourRangeUtils.hourRangeSetToTimeRangeSet
+import org.j4work.kotlin.utils.string.HourRangeUtils.isTimeRange
+import org.j4work.kotlin.utils.string.HourRangeUtils.parseTimeRangeSet
+import org.j4work.kotlin.utils.string.HourRangeUtils.timeRangeSetToHourRangeSet
 import org.junit.jupiter.api.Test
+import java.time.LocalTime
 
 class HourRangeUtilsTest {
+    @Test
+    fun `is time range explicit 1am to explicit 6am`() {
+        assertThat(isTimeRange("01-06"))
+            .isTrue()
+    }
 
     @Test
-    fun `explicit 1am to explicit 6am`() {
-        assertThat(parseHourRangeSet("01-06"))
+    fun `is time range explicit 0930am to explicit 0315pm`() {
+        assertThat(isTimeRange("09.30-15.15"))
+            .isTrue()
+    }
+
+    @Test
+    fun `is time range explicit 10am to explicit 12pm and implicit 15pm to implicit 20pm`() {
+        assertThat(isTimeRange("10-12/03-08"))
+            .isTrue()
+    }
+
+    @Test
+    fun `is time range explicit 10am to explicit 12pm and implicit 1515pm to implicit 2045pm`() {
+        assertThat(isTimeRange("10-12/03.15-08.45"))
+            .isTrue()
+    }
+
+    @Test
+    fun `is time range test with spaces`() {
+        assertThat(isTimeRange(" 10 - 01.15 / 05 - 08 "))
+            .isTrue()
+    }
+
+    @Test
+    fun `convert explicit 1am to explicit 6am`() {
+        assertThat(timeRangeSetToHourRangeSet(TreeRangeSets.closedOpen(LocalTime.parse("01:00")..LocalTime.parse("06:00"))))
             .isEqualTo(TreeRangeSets.closedOpen(1..6))
     }
 
     @Test
-    fun `implicit 1pm to implicit 6pm because of min start hour`() {
-        assertThat(parseHourRangeSet("01-06", 8))
-            .isEqualTo(TreeRangeSets.closedOpen(13..18))
-    }
-
-    @Test
-    fun `explicit 10am to implicit 6pm because end hour is smaller than start`() {
-        assertThat(parseHourRangeSet("10-06"))
-            .isEqualTo(TreeRangeSets.closedOpen(10..18))
-    }
-
-    @Test
-    fun `explicit 9am to explicit 3pm`() {
-        assertThat(parseHourRangeSet("09-15", 8))
-            .isEqualTo(TreeRangeSets.closedOpen(9..15))
-    }
-
-    @Test
-    fun `explicit 10am to explicit 12pm and implicit 15pm to implicit 20pm`() {
-        assertThat(parseHourRangeSet("10-12/03-08", 8))
+    fun `convert explicit 10am to explicit 12pm and implicit 1515pm to implicit 2045pm`() {
+        assertThat(timeRangeSetToHourRangeSet(TreeRangeSets.closedOpen(LocalTime.parse("10:00")..LocalTime.parse("12:00"),
+            LocalTime.parse("15:15")..LocalTime.parse("20:45"))))
             .isEqualTo(TreeRangeSets.closedOpen(10..12, 15..20))
     }
 
     @Test
-    fun `explicit 10am to implicit 13pm and implicit 17pm to implicit 20pm`() {
-        assertThat(parseHourRangeSet("10-01/05-08", 8))
-            .isEqualTo(TreeRangeSets.closedOpen(10..13, 17..20))
+    fun `reverse convert explicit 1am to explicit 6am`() {
+        assertThat(hourRangeSetToTimeRangeSet(TreeRangeSets.closedOpen(1..6)))
+            .isEqualTo(TreeRangeSets.closedOpen(LocalTime.parse("01:00")..LocalTime.parse("06:00")))
     }
 
     @Test
-    fun `test with spaces`() {
-        assertThat(parseHourRangeSet(" 10 - 01 / 05 - 08 ", 8))
-            .isEqualTo(TreeRangeSets.closedOpen(10..13, 17..20))
+    fun `reverse convert explicit 10am to explicit 12pm and implicit 1515pm to implicit 2045pm`() {
+        assertThat(hourRangeSetToTimeRangeSet(TreeRangeSets.closedOpen(10..12, 15..20)))
+            .isEqualTo(TreeRangeSets.closedOpen(LocalTime.parse("10:00")..LocalTime.parse("12:00"),
+                LocalTime.parse("15:00")..LocalTime.parse("20:00")))
+    }
+
+    @Test
+    fun `parse time range explicit 1am to explicit 6am`() {
+        assertThat(parseTimeRangeSet("01-06"))
+            .isEqualTo(TreeRangeSets.closedOpen(LocalTime.parse("01:00")..LocalTime.parse("06:00")))
+    }
+
+    @Test
+    fun `parse time range implicit 1pm to implicit 6pm because of min start hour`() {
+        assertThat(parseTimeRangeSet("01-06", 8))
+            .isEqualTo(TreeRangeSets.closedOpen(LocalTime.parse("13:00")..LocalTime.parse("18:00")))
+    }
+
+    @Test
+    fun `parse time range explicit 10am to implicit 6pm because end hour is smaller than start`() {
+        assertThat(parseTimeRangeSet("10-06"))
+            .isEqualTo(TreeRangeSets.closedOpen(LocalTime.parse("10:00")..LocalTime.parse("18:00")))
+    }
+
+    @Test
+    fun `parse time range explicit 9am to explicit 3pm`() {
+        assertThat(parseTimeRangeSet("09-15", 8))
+            .isEqualTo(TreeRangeSets.closedOpen(LocalTime.parse("09:00")..LocalTime.parse("15:00")))
+    }
+
+    @Test
+    fun `parse time range explicit 0930am to explicit 0315pm`() {
+        assertThat(parseTimeRangeSet("09.30-15.15", 8))
+            .isEqualTo(TreeRangeSets.closedOpen(LocalTime.parse("09:30")..LocalTime.parse("15:15")))
+    }
+
+    @Test
+    fun `parse time range explicit 10am to explicit 12pm and implicit 15pm to implicit 20pm`() {
+        assertThat(parseTimeRangeSet("10-12/03-08", 8))
+            .isEqualTo(TreeRangeSets.closedOpen(LocalTime.parse("10:00")..LocalTime.parse("12:00"),
+                LocalTime.parse("15:00")..LocalTime.parse("20:00")))
+    }
+
+    @Test
+    fun `parse time range explicit 10am to explicit 12pm and implicit 1515pm to implicit 2045pm`() {
+        assertThat(parseTimeRangeSet("10-12/03.15-08.45", 8))
+            .isEqualTo(TreeRangeSets.closedOpen(LocalTime.parse("10:00")..LocalTime.parse("12:00"),
+                LocalTime.parse("15:15")..LocalTime.parse("20:45")))
+    }
+
+    @Test
+    fun `parse time range explicit 10am to implicit 13pm and implicit 17pm to implicit 20pm`() {
+        assertThat(parseTimeRangeSet("10-01/05-08", 8))
+            .isEqualTo(TreeRangeSets.closedOpen(LocalTime.parse("10:00")..LocalTime.parse("13:00"),
+                LocalTime.parse("17:00")..LocalTime.parse("20:00")))
+    }
+
+    @Test
+    fun `parse time range test with spaces`() {
+        assertThat(parseTimeRangeSet(" 10 - 01.15 / 05 - 08 ", 8))
+            .isEqualTo(TreeRangeSets.closedOpen(LocalTime.parse("10:00")..LocalTime.parse("13:15"),
+                LocalTime.parse("17:00")..LocalTime.parse("20:00")))
     }
 }
